c2c148ac53034763b24e2988298a108dea932b11
rchand7 <150305815+rchand7@users.noreply.github.com>
2024-11-29 05:24:33 +0530

    Update company.model.js

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -1,27 +1,67 @@
 import mongoose from "mongoose";
 
-const companySchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true
+const jobSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
     },
-    description:{
-        type:String, 
+    description: {
+        type: String,
+        required: true
     },
-    website:{
-        type:String 
+    requirements: [{
+        type: String
+    }],
+    salary: {
+        type: String, // Changed from Number to String to accept alphanumeric values
+        required: true,
+        validate: {
+            validator: function(value) {
+                // Custom validation to allow both numbers and alphabets
+                return /^[a-zA-Z0-9\s]*$/.test(value);  // Accepts alphanumeric values and spaces
+            },
+            message: 'Salary must be alphanumeric.'
+        }
     },
-    location:{
-        type:String 
+    experienceLevel: {
+        type: String, // Changed from Number to String to accept alphanumeric values
+        required: true,
+        validate: {
+            validator: function(value) {
+                // Custom validation to allow both numbers and alphabets
+                return /^[a-zA-Z0-9\s]*$/.test(value);  // Accepts alphanumeric values and spaces
+            },
+            message: 'Experience Level must be alphanumeric.'
+        }
     },
-    logo:{
-        type:String // URL to company logo
+    location: {
+        type: String,
+        required: true
     },
-    userId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
-    }
-},{timestamps:true})
-export const Company = mongoose.model("Company", companySchema);
+    jobType: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: Number,
+        required: true
+    },
+    company: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
+    },
+    created_by: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    applications: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Application',
+        }
+    ]
+}, {timestamps: true});
+
+export const Job = mongoose.model("Job", jobSchema);
